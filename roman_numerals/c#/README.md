56f8c4fa41302107233f95f104f59a6923b90fe7
Jibidus <jibidus@gmail.com>
2019-11-22 13:34:13 +0100

    [ROMAN_NUMERALS] Add C# support

---
+++ roman_numerals/c#/README.md
@@ -0,0 +1,11 @@
+
+# How to start?
+
+1- Install [Visual Studio](https://visualstudio.microsoft.com)
+
+2- Open `RomanNumerals.sln` with `Visual Studio`
+
+3- Run `RomanNumeralTest`
+
+4- Hack `RomanNumeral.cs`
+
---
+++ roman_numerals/c#/RomanNumerals.cs
@@ -0,0 +1,20 @@
+using System;
+
+class RomanNumerals
+{
+    public static void Main(string[] args)
+    {
+        int count = -1;
+        int x;
+        do
+        {
+            x = Console.Read();
+            count += 1;
+        } while (x != -1);
+
+        if (count != 0)
+        {
+            Console.Write(count);
+        }
+    }
+}
