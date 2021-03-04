6f153c769e14d9a5ba00891fb5519b22dfd30b5e
ahmdIbrhm <ahmadalibrahim28@gmail.com>
2021-03-04 17:29:28 +0100

    6th test

--- reverse_polish_notation/python/rpn.py
+++ reverse_polish_notation/python/rpn.py
@@ -6,15 +6,14 @@ def main():
   for line in sys.stdin:
     operator = findOperator(line)
     elements = re.split('\+|-',line)
-    element0 = int(elements[0])
+    elements = list(map(int, elements))
     if operator == "none":
-      print(element0)
+      print(elements[0])
     else:
-      element1 = int(elements[1])
       if operator == '+':
-        print(element0+element1)
+        print(sum(elements))
       elif operator == '-': 
-        print(element0-element1)
+        print(elements[0]-elements[1])
 
 def findOperator(line):
   if line.find("+") != -1:
