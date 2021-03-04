d6d6c0f4a108214a7c05460084c1e1bda6728803
ahmdIbrhm <ahmadalibrahim28@gmail.com>
2021-03-04 16:47:20 +0100

    4th test

--- reverse_polish_notation/python/rpn.py
+++ reverse_polish_notation/python/rpn.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 import sys
-
+import re
 def main():
   for line in sys.stdin:
-    elements = line.split("+")
+    operator = line[1]
+    elements = re.split('\+|-',line)
     element0 = int(elements[0])
     if len(elements) == 1:
       print(element0)
     else:
       element1 = int(elements[1])
-      print(element0+element1)
+      if operator == '+':
+        print(element0+element1)
+      elif operator == '-': 
+        print(element0-element1)
 
 
 if __name__== "__main__":
