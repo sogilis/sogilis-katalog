65cd80bfea1c0ac18bbac2441cf098bf1cfe71c6
ahmdIbrhm <ahmadalibrahim28@gmail.com>
2021-03-04 16:19:16 +0100

    First and second test

--- reverse_polish_notation/python/rpn.py
+++ reverse_polish_notation/python/rpn.py
@@ -2,26 +2,10 @@
 
 import sys
 
-operators = {
-   "+": lambda stack: stack.pop() + stack.pop(),
-   "-": lambda stack: -stack.pop() + stack.pop(),
-   "/": lambda stack: 1 / (stack.pop() / stack.pop())
-}
-
 def main():
-  stack = []
-
   for line in sys.stdin:
-    elements = line.rstrip('\n').split(" ")
-    
-  for element in elements: 
-    if element in operators:
-      result = operators[element](stack)
-    else:
-      result = element
-    stack.append(int(result))
-  
-  print(stack.pop())
+    print(line)
+
 
 if __name__== "__main__":
   main()
