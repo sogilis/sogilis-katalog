e8dc168db41a95113345bb3633888884a3b40031
Clement Pajean <clement@sogilis.com>
2021-03-17 17:36:09 +0100

    ipa

--- reverse_polish_notation/python/rpn.py
+++ reverse_polish_notation/python/rpn.py
@@ -23,7 +23,7 @@ def eval(expression):
   else:
     elements = expression.split(operator, 1)
     if is_unary_operator(elements):
-      return float(-2)
+      return -float(elements[1])
     else:
       if operator == '+':
         return eval(elements[0])+eval(elements[1])
